e3835d7dfe64929bf9efac86f5b8efc94fc78037
Lucas Ricarte <lucasrrt@gmail.com>
2018-10-07 12:02:14 -0300

    scope

---
+++ scope_functions.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//TODO what is the max nest level
+#define MAX_NEST_LEVEL 10
+
+typedef struct object
+{
+    int nName;
+    struct object *pNext;
+} object, *pobject;
+
+pobject SymbolTable[MAX_NEST_LEVEL];
+pobject SymbolTableLast[MAX_NEST_LEVEL];
+int nCurrentLevel = -1;
+
+int NewBlock(void)
+{
+    SymbolTable[++nCurrentLevel] = NULL;
+    SymbolTableLast[nCurrentLevel] = NULL;
+    return nCurrentLevel;
+}
+
+int EndBlock(void)
+{
+    return --nCurrentLevel;
+}
+
+pobject Define(int aName)
+{
+    pobject obj = (pobject)malloc(sizeof(object));
+    obj->nName = aName;
+    obj->pNext = NULL;
+    if (SymbolTable[nCurrentLevel] == NULL)
+    {
+        SymbolTable[nCurrentLevel] = obj;
+        SymbolTableLast[nCurrentLevel] = obj;
+    }
+    else
+    {
+        SymbolTableLast[nCurrentLevel]->pNext = obj;
+        SymbolTableLast[nCurrentLevel] = obj;
+    }
+    return obj;
+}
+
+pobject Search(int aName)
+{
+    pobject obj = SymbolTable[nCurrentLevel];
+    while (obj != NULL)
+    {
+        if (obj->nName == aName)
+            break;
+        else
+            obj = obj->pNext;
+    }
+    return obj;
+}
+
+pobject Find(int aName)
+{
+    int i;
+    pobject obj = NULL;
+    for (i = nCurrentLevel; i >= 0; --i)
+    {
+        obj = SymbolTable[i];
+        while (obj != NULL)
+        {
+            if (obj->nName == aName)
+                break;
+            else
+                obj = obj->pNext;
+        }
+        if (obj != NULL)
+            break;
+    }
+    return obj;
+}
